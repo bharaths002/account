2a067c1d83a6b3c6e8a125df7684545148213786
Bharath S <mailmehere1820@gmail.com>
2025-08-25 13:09:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.js';
 
 const app = express();
 
-app.use(cors({ origin: 'https://[your-frontend-service-name].onrender.com' }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
